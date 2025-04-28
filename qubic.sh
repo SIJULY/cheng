72386a15f5ae293f733a00c5b641e2264b201dbd
SIJULY <104404467+SIJULY@users.noreply.github.com>
2025-04-28 14:32:03 +0800

    Update qubic.sh

--- qubic.sh
+++ qubic.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-WORKER=$(curl -s https://api.ipify.org)
+# 直接在这里设置你想要的名字！
+WORKER="sijuly" 
 
-if [ -n "$CUSTOM_WORKER" ]; then
-    WORKER=$CUSTOM_WORKER
-fi
+# 既然已经固定了名字，就不再需要 curl 和下面的 if 判断了
+# WORKER=$(curl -s https://api.ipify.org) 
+# if [ -n "$CUSTOM_WORKER" ]; then
+#     WORKER=sijuly
+# fi
 
 # 直接尝试启动，跳过检查
 nohup ./apoolminer --account CP_0codi1cvup --worker $WORKER --gpu-off --pool qubic1.hk.apool.io:3334 >> qubic.log 2>&1 &
